0a32f250bb2434fba4123f45a32e2a4c0eb09f77
JeeyaSharma <jeeyas39@gmail.com>
2024-10-12 16:10:59 +0530

    Day 1 of Learning Javascript

---
+++ Day-1/script.js
@@ -0,0 +1,5 @@
+// alert('hello');
+
+console.log(2+2);
+console.log('Heyloooo!');
+console.log('Welcome!');
---
+++ Day-1/variables.js
@@ -0,0 +1,20 @@
+let var1=3;
+console.log(var1);
+
+let calculation=2+4;
+console.log(calculation);
+console.log(calculation+2);
+
+let result=calculation + 2 ;
+console.log(result);
+
+let message = 'hello';
+console.log(message);
+
+var1=5;
+console.log(var1);
+
+var1='Jeeya';
+console.log(var1);
+
+console.log(typeof(var1));
